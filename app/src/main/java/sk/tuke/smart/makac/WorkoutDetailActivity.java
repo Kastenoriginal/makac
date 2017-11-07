e97ca567a915fbf3bcd8d6e93aa1208f941fd0a8
SYNAPSE\patrik.varga <patrik.varga>
2017-11-07 14:28:41 +0100

    Implemented email sending.

--- app/src/main/java/sk/tuke/smart/makac/WorkoutDetailActivity.java
+++ app/src/main/java/sk/tuke/smart/makac/WorkoutDetailActivity.java
@@ -1,7 +1,11 @@
 package sk.tuke.smart.makac;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import butterknife.OnClick;
 
 public class WorkoutDetailActivity extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class WorkoutDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout_detail);
+    }
+
+    @OnClick(R.id.button_workoutdetail_gplusshare)
+    void shareGooglePlus() {
+        // TODO: 11/7/2017
+    }
+
+    @OnClick(R.id.button_workoutdetail_twittershare)
+    void shareTwitter() {
+        // TODO: 11/7/2017
+    }
+
+    @OnClick(R.id.button_workoutdetail_emailshare)
+    void sendEmail() {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"recipient@example.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+        i.putExtra(Intent.EXTRA_TEXT   , "body of email");
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(WorkoutDetailActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @OnClick(R.id.button_workoutdetail_fbsharebtn)
+    void shareFacebook() {
+        // TODO: 11/7/2017
     }
 }
